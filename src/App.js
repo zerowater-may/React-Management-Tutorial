dc5da8ff49055d2cc2d9d72fc8627b05b1eb4b0d
kim young su <finally1ad@gmail.com>
2020-05-01 21:10:19 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer'
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
-}
+const customers = [
+{
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : ' 나동빈 ',
+  'birthday' : '961222',
+  'gender' : '남자',
+  'job' : '대학생'
+},
+{
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : ' 나동빈빈 ',
+  'birthday' : '96122dd2',
+  'gender' : '남자',
+  'job' : '대학생'
 
+},
+{
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : ' 나동진 ',
+  'birthday' : '9612d22',
+  'gender' : '남자여자',
+  'job' : '대학생생'
+
+}
+]
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.name} gender={c.gender} job={c.job}/>); } ) }
+      </div>
+    );
+  }
+}
 export default App;
